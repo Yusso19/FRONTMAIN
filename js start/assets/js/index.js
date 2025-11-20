3dd6ad9b42ccb4c315b7ba86b761644aa3235dcc
Yusuf T. Mammadov <yusuftm@code.local>
2025-11-20 17:39:04 +0400

    added

---
+++ js start/assets/js/index.js
@@ -0,0 +1,43 @@
+ 
+console.log("hello guys how are you");  
+// let n = 1;
+
+// if (n % 3 === 0 && n % 7 === 0) {
+//     console.log("Həm 3-ə, həm də 7-yə bölünür");
+// } else {
+//     console.log("Bölünmür");
+// }
+
+// 2 primer 
+
+// let a = 5;
+// let b = 10;
+// let count = 0;
+
+// for (let i = a; i <= b; i++)
+// {
+//     if(i%2 !== 0){
+//         count++;
+        
+//     }
+
+// console.log(count);
+// }
+
+// 3 primer
+// let a = 5;
+// let b = 10;
+// let sum = 0;
+
+// for (let i = a; i <= b; i++)
+// {
+//     if(i%2 !== 0){
+//         sum += i;
+        
+//     }
+
+
+// }
+// console.log(sum);
+
+// 4 primer
